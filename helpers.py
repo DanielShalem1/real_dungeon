52784087092aff9a40b451b7fe4ad0865a423a89
DanielShalem1 <DanielShalem1@gmail.com>
2020-05-10 13:57:32 +0300

    added documentation

--- helpers.py
+++ helpers.py
@@ -5,10 +5,15 @@ def name_pick(creature):
         return creature.name
 
 def able_to_attack(attacker):
+    """
+    :param attacker: the next creature to attack
+    checks if the next creature to attack has a status preventing it from attacking
+    :return: True if tha attacker can attack and False if not
+    """
     statuses = list(attacker.status.keys())
-    print(statuses)
-    bad_statuses = ["petrified"]
-    if any([y in bad_statuses for y in statuses]):
+
+    bad_statuses = ["petrified"] # list of statuses which prevent attacking
+    if any([y in bad_statuses for y in statuses]): #checks if one if one of the attacker's statuses is in the bad_statuses list
         return False
     else:
         return True
